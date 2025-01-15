20c945e7271ac71c8ddd532ebac6228553bf5216
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2025-01-15 10:56:56 +0300

    Update streamRecorder.js

--- streamRecorder.js
+++ streamRecorder.js
@@ -1,7 +1,5 @@
-const ffmpeg = require('fluent-ffmpeg');
+const { spawn } = require('child_process');
 const ffmpegPath = require('ffmpeg-static');
-
-ffmpeg.setFfmpegPath(ffmpegPath);
 
 class StreamRecorder {
     constructor(outputFile) {
@@ -12,41 +10,38 @@ class StreamRecorder {
 
     init() {
         console.log("Initializing FFmpeg process for MJPEG...");
-        this.ffmpegProcess = ffmpeg({ source: 'pipe:0' }) // Указываем, что входной поток будет из stdin
-            .inputFormat('mjpeg') // Указываем формат входного потока
-            .output(this.outputFile)
-            .audioCodec('aac') // Если необходимо, можно добавить аудио
-            .videoCodec('libx264')
-            .format('mp4') // Выходной формат
-            .on('start', commandLine => {
-                console.log('FFmpeg process started: ' + commandLine);
-            })
-            .on('error', err => {
-                console.error('Error during FFmpeg processing: ' + err.message);
-            })
-            .on('end', () => {
-                console.log('Recording finished successfully');
-            })
-            .run();
 
-        // Проверка наличия stdin
-        if (this.ffmpegProcess && this.ffmpegProcess.stdin) {
-            console.log("FFmpeg stdin is available.");
-        } else {
-            console.error("FFmpeg process is not initialized or stdin is missing.");
-        }
+        // Запускаем ffmpeg процесс
+        this.ffmpegProcess = spawn(ffmpegPath, [
+            '-f', 'mjpeg',         // Указываем формат входного потока
+            '-i', 'pipe:0',       // Входные данные из stdin
+            '-c:v', 'libx264',    // Кодек видео
+            '-preset', 'fast',     // Предустановка для кодирования
+            '-crf', '23',          // Качество видео
+            '-pix_fmt', 'yuv420p', // Формат пикселей
+            '-movflags', 'faststart', // Оптимизация для веб-потока
+            this.outputFile        // Выходной файл
+        ]);
+
+        this.ffmpegProcess.stdout.on('data', (data) => {
+            console.log(`FFmpeg stdout: ${data}`);
+        });
+
+        this.ffmpegProcess.stderr.on('data', (data) => {
+            console.error(`FFmpeg stderr: ${data}`);
+        });
+
+        this.ffmpegProcess.on('close', (code) => {
+            console.log(`FFmpeg process exited with code ${code}`);
+        });
     }
 
     write(data) {
-        if (this.ffmpegProcess && this.ffmpegProcess.stdin) {
-            if (this.ffmpegProcess.stdin.writable) {
-                console.log('Writing data to ffmpeg stdin');
-                this.ffmpegProcess.stdin.write(data);
-            } else {
-                console.error('FFmpeg stdin is not writable');
-            }
+        if (this.ffmpegProcess.stdin.writable) {
+            console.log('Writing data to ffmpeg stdin');
+            this.ffmpegProcess.stdin.write(data);
         } else {
-            console.error('FFmpeg process is not initialized or stdin is missing');
+            console.error('FFmpeg stdin is not writable');
         }
     }
 
@@ -59,4 +54,5 @@ class StreamRecorder {
 }
 
 module.exports = StreamRecorder;
+
 
