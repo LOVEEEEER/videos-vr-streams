7f2831cd0d8280763e527f6e8edf8cc4e8b108bc
Demionchek <86489386+Demionchek@users.noreply.github.com>
2025-01-24 15:09:21 +0300

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -1,55 +1,85 @@
 const fs = require('fs');
 const path = require('path');
+const { createCanvas, Image } = require('canvas');
 const JPEG = require('jpeg-js');
+const { Transform } = require('stream');
 
 class GameViewDecoder {
   constructor(outputPath) {
     this.outputPath = outputPath;
+    this.frameBuffer = Buffer.alloc(650000); // initial buffer size
+    this.frameIdx = 0;
+    this.inPicture = false;
+    this.previous = 0;
+    this.current = 0;
+    this.streamLength = 0;
+    this.canvas = createCanvas(1, 1);
+    this.context = this.canvas.getContext('2d');
     this.videoStream = fs.createWriteStream(outputPath);
-    this.label = 1001;
-    this.dataID = 0;
-    this.dataLength = 0;
-    this.receivedLength = 0;
-    this.dataByte = null;
-    this.isGZipped = false;
-    this.ColorReductionLevel = 0;
-    this.isDesktopFrame = false;
   }
 
   async processImageData(data) {
     const imageData = JPEG.decode(data, true);
+    this.canvas.width = imageData.width;
+    this.canvas.height = imageData.height;
     this.context.putImageData(new ImageData(imageData.data, imageData.width, imageData.height), 0, 0);
 
+    // Save each frame to the video stream
+    const stream = this.canvas.createJPEGStream();
     stream.pipe(this.videoStream, { end: false });
   }
 
-  async Action_ProcessImageData(_byteData) {
-    if (_byteData.length <= 14) return;
+  async processMJPEGData(data) {
+    this.parseStreamBuffer(data);
+  }
 
-    const _label = _byteData.readUInt16LE(0);
-    if (_label != this.label) return;
+  parseStreamBuffer(streamBuffer) {
+    let idx = 0;
+    this.streamLength = streamBuffer.length;
 
-    const _dataID = _byteData.readUInt16LE(2);
-    if (_dataID != this.dataID) this.receivedLength = 0;
-    this.dataID = _dataID;
-
-    const dataLength = _byteData.readInt32LE(4);
-    const _offset = _byteData.readInt32LE(8);
-    this.isGZipped = _byteData[12] === 1;
-    this.ColorReductionLevel = _byteData[13];
-
-    this.isDesktopFrame = _byteData[14] !== 0;
-    const metaByteLength = this.isDesktopFrame ? 24 : 15;
-
-    if (this.receivedLength === 0) this.dataByte = Buffer.alloc(dataLength);
-    const chunkLength = _byteData.length - metaByteLength;
-    if (_offset + chunkLength <= this.dataByte.length) {
-      this.receivedLength += chunkLength;
-      _byteData.copy(this.dataByte, _offset, metaByteLength);
+    while (idx < this.streamLength) {
+      if (this.inPicture) {
+        this.parsePicture(streamBuffer, idx);
+      } else {
+        this.searchPicture(streamBuffer, idx);
+      }
+      idx++;
     }
+  }
 
-    if (this.receivedLength === dataLength) {
-      await this.processImageData(this.dataByte);
+  searchPicture(streamBuffer, idx) {
+    while (idx < this.streamLength) {
+      this.previous = this.current;
+      this.current = streamBuffer[idx++];
+
+      if (this.previous === 0xFF && this.current === 0xD8) {
+        this.frameIdx = 2;
+        this.frameBuffer[0] = 0xFF;
+        this.frameBuffer[1] = 0xD8;
+        this.inPicture = true;
+        return;
+      }
+    }
+  }
+
+  parsePicture(streamBuffer, idx) {
+    while (idx < this.streamLength) {
+      this.previous = this.current;
+      this.current = streamBuffer[idx++];
+
+      if (this.frameIdx >= this.frameBuffer.length) {
+        const newBuffer = Buffer.alloc(this.frameBuffer.length * 2);
+        this.frameBuffer.copy(newBuffer);
+        this.frameBuffer = newBuffer;
+      }
+
+      this.frameBuffer[this.frameIdx++] = this.current;
+
+      if (this.previous === 0xFF && this.current === 0xD9) {
+        this.processImageData(this.frameBuffer.slice(0, this.frameIdx));
+        this.inPicture = false;
+        return;
+      }
     }
   }
 
